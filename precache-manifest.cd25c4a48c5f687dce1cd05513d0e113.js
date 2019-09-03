211bebdc6a6ddb96e0f7b0b696a365e01cd35af4
Akriti Bhusal <akriti.ab@gmail.com>
2019-09-03 15:57:22 +1000

    Add first build

---
+++ precache-manifest.cd25c4a48c5f687dce1cd05513d0e113.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bd8e124006f6dd50e32c9cfc019591f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c4aeaf28f0240ab94bdc",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "798cb6012dae37596679",
+    "url": "/static/js/2.526fee24.chunk.js"
+  },
+  {
+    "revision": "c4aeaf28f0240ab94bdc",
+    "url": "/static/js/main.91c8ea18.chunk.js"
+  },
+  {
+    "revision": "de463be5d5aa0ad86ed5",
+    "url": "/static/js/runtime~main.d37a8383.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
